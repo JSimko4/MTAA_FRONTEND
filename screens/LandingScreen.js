ac73e654008a8f232823a282d912f69c70f331b1
StefanRapco <stefanrapco@gmail.com>
2022-04-10 19:34:58 +0200

    added LandingPage & refactor of components

---
+++ screens/LandingScreen.js
@@ -0,0 +1,73 @@
+//import { View, Button, Text } from 'react-native';
+
+
+import React, { Component } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import RegistrationButtonOnLandingScreen from "../components/RegistrationButtonLandingScreen";
+import LoginButtonOnLandingScreen from "../components/LoginButtonLandingScreen";
+
+export default function LandingScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/273945634_447635370382438_7338247850667943623_n.jpg")}
+        resizeMode="contain"
+        style={styles.image1}
+      ></Image>
+      <View style={styles.image1Filler}></View>
+      <View style={styles.LoginRegisterColumn}>
+        <RegistrationButtonOnLandingScreen
+          style={styles.RegistrationButtonStyle}
+        ></RegistrationButtonOnLandingScreen>
+        <LoginButtonOnLandingScreen
+          style={styles.LoginButtonStyle}
+        ></LoginButtonOnLandingScreen>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  image1: {
+    width: 285,
+    height: 269,
+    marginTop: 48,
+    marginLeft: 50
+  },
+  image1Filler: {
+    flex: 1
+  },
+  RegistrationButtonStyle: {
+    width: 218,
+    height: 47,
+    borderRadius: 100,
+    backgroundColor: "rgba(21,9,65,1)",
+    marginBottom: -146
+  },
+  LoginButtonStyle: {
+    width: 218,
+    height: 47,
+    borderRadius: 100,
+    backgroundColor: "rgba(21,9,65,1)",
+    marginBottom: -146
+  },
+  LoginRegisterColumn: {
+    width: 218,
+    marginBottom: 150,
+    marginLeft: 85
+  }
+});
+
+/*export default function HomeScreen({navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>HOME SCREEN</Text>
+        <Button title="Login" onPress={() => navigation.push("Login")} />
+        <Button title="Register" onPress={() => navigation.push("Registration")} />
+      </View>
+    );
+  }*/
+
