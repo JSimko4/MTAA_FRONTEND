7e97a1caaf47cd7df2802bcd4beba84fedfec761
JSimko4 <xsimkoj2@stuba.sk>
2022-04-10 18:37:34 +0200

    login request

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,8 @@ import { View, Button, Text } from 'react-native';
 export default function HomeScreen({navigation}) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>HOME SCREEN</Text><Button title="Sign in" onPress={() => alert("todo!")} />
+        <Text>HOME SCREEN</Text>
+        <Button title="Login" onPress={() => navigation.push("Login")} />
         <Button title="Register" onPress={() => navigation.push("Registration")} />
       </View>
     );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,40 @@
 import { View, Button, Text } from 'react-native';
+import globals from '../globals'
+
+const loginApi = ({navigation}) => {
+  return fetch(globals.API_URL + 'login/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: "email",
+      password: "password"
+    })
+  })
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+
+    if(json['access_token'] !== undefined){
+      navigation.navigate('Home', {access_token: json['access_token']})
+    }
+    else{
+      alert("nespravne udaje")
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+};
 
 export default function LoginScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
     <Text>Login screen</Text>
-    <Button title="Sign up" onPress={() => alert("todo!")} />
+    <Button title="Sign up" onPress={() => loginApi({navigation})} />
     </View>
   );
 }
