3b8dfe2dd9c6d0ce5db9855065677b4ba776f2ac
JSimko4 <xsimkoj2@stuba.sk>
2022-04-11 14:13:27 +0200

    exercises page

--- screens/ExercisesScreen.js
+++ screens/ExercisesScreen.js
@@ -1,31 +1,36 @@
 
 import * as React from 'react';
-import { View, StyleSheet, Button, Image, FlatList, Text } from 'react-native';
+import { View, StyleSheet, Button, Image, FlatList, Text, TouchableOpacity } from 'react-native';
 
 export default function ExercisesScreen({route, navigation}) {
-    const { exercises } = route.params;
-    console.log(exercises)
-    console.log(route.params)
-    
+    const exercises = route.params.exercises;
+
     const styles = StyleSheet.create({
     });
   
     return (
         <FlatList
             data={exercises}
-            keyExtractor={item => item.id}
-            renderItem={( item ) => (
-                <View>
-                    {console.log(item)}
-                    <Text>{item.name}</Text>
-                    <Image
-                        style={{width: '200px', height: '200px'}}
-                        source={{uri: global.API_URL+item.image_path}}
-                    />
-                    <Button
-                        title='Detail'
+            renderItem={( {item} ) => (
+                <View style ={{ flexDirection:'row', flex:1, justifyContent:'space-evenly', alignItems:'center',
+                                marginTop: 20}}>
+                    <TouchableOpacity  
+                        style ={{flex:3, alignItems:'center', justifyContent:'center'}}
                         onPress={() => navigation.navigate("ExerciseDetail", {exercise: item})}
-                    />
+                    >
+                        <Image
+                            style={{width: 110, height: 110}}
+                            source={{uri: global.API_URL+item.image_path}}
+                        />
+                    </TouchableOpacity >
+                    
+                    <TouchableOpacity 
+                        style ={{flexDirection:'row', flex:5, alignItems:'center', justifyContent:'center'}}
+                        onPress={() => navigation.navigate("ExerciseDetail", {exercise: item})}
+                    >
+                    <Text style ={{fontSize: 19, fontWeight: 'bold'}}
+                    >{item.name.toUpperCase()}</Text>
+                    </TouchableOpacity>
                 </View>
             )}
         />
